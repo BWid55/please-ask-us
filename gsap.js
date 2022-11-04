0cb92d4b209ef4f7fb226f78f6bf4f0b3f39d81b
BWid55 <brendan.widmer@gmail.com>
2022-11-03 22:10:29 -0500

    update

--- gsap.js
+++ gsap.js
@@ -1,7 +1,46 @@
-console.log('hello')
-
 gsap.from("#header", {
-    opacity: 0, 
-    y: -100, 
-    duration: 1
-  });
+  opacity: 0,
+  y: -100,
+  duration: 1,
+  clearProps: "transform",
+});
+
+gsap.from(".frame-corner-top-left", {
+  opacity: 0,
+  y: -50,
+  x: -50,
+  duration: 1,
+});
+gsap.from(".frame-corner-top-right", {
+  opacity: 0,
+  y: -50,
+  x: 50,
+  duration: 1,
+});
+gsap.from(".frame-corner-bottom-left", {
+  opacity: 0,
+  y: 50,
+  x: -50,
+  duration: 1,
+});
+gsap.from(".frame-corner-bottom-right", {
+  opacity: 0,
+  y: 50,
+  x: 50,
+  duration: 1,
+});
+gsap.from(".frame-side-left", {
+  opacity: 0,
+  x: -200,
+  duration: 1,
+});
+gsap.from(".frame-side-right", {
+  opacity: 0,
+  x: 200,
+  duration: 1,
+});
+gsap.from("#content p", {
+  opacity: 0,
+  duration: 1,
+  delay: 2,
+});
