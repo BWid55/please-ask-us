815c20b2093495facf338cd78c276c21c0ff55c7
BWid55 <brendan.widmer@gmail.com>
2022-11-03 22:49:30 -0500

    update

--- gsap.js
+++ gsap.js
@@ -9,38 +9,39 @@ gsap.from(".frame-corner-top-left", {
   opacity: 0,
   y: -50,
   x: -50,
-  duration: 1,
+  duration: 1.25,
 });
 gsap.from(".frame-corner-top-right", {
   opacity: 0,
   y: -50,
   x: 50,
-  duration: 1,
+  duration: 1.25,
 });
 gsap.from(".frame-corner-bottom-left", {
   opacity: 0,
   y: 50,
   x: -50,
-  duration: 1,
+  duration: 1.25,
 });
 gsap.from(".frame-corner-bottom-right", {
   opacity: 0,
   y: 50,
   x: 50,
-  duration: 1,
+  duration: 1.25,
 });
 gsap.from(".frame-side-left", {
   opacity: 0,
   x: -200,
-  duration: 1,
+  duration: 1.25,
 });
 gsap.from(".frame-side-right", {
   opacity: 0,
   x: 200,
-  duration: 1,
+  duration: 1.25,
 });
 gsap.from("#content p", {
   opacity: 0,
   duration: 1,
-  delay: 2,
+  y: 10,
+  delay: 1,
 });
